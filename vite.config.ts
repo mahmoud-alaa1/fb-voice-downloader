c903fa690a222c5a3a40ff599bd7c46001701659
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2026-04-07 14:00:03 +0200

    Bundler Configuration
    Used CRJX plugin and added icons for the extension

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,7 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./manifest.json";
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        background: resolve(__dirname, "src/background/index.ts"),
-        content: resolve(__dirname, "src/content/index.ts"),
-        // popup: resolve(__dirname, "src/popup/index.html"),
-      },
-      output: {
-        entryFileNames: "[name].js",
-        chunkFileNames: "chunks/[name].js",
-        assetFileNames: "assets/[name].[ext]",
-      },
-    },
-    outDir: "dist",
-    emptyOutDir: true,
-  },
+  plugins: [crx({ manifest })],
 });
