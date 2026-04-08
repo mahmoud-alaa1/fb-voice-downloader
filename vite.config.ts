f398b5a16486aacdbdf9bb656f1d8b1f87e91eae
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2026-04-08 13:53:16 +0200

    feat: add CSS variables for dark and light modes and update button styles

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import manifest from "./manifest.json";
 
 export default defineConfig({
   plugins: [crx({ manifest })],
+  resolve: {
+    alias: {
+      "@content": "/src/content",
+      "@utils": "/src/utils",
+      "@types": "/src/types",
+    },
+  },
 });
